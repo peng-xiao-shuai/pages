c0cc99a7646029bca5ff57fd12841b8ddcd77764
Peng_Xiao_Shuai_0902 <1612565136@qq.com>
2021-10-31 16:11:57 +0800

    首页完成

---
+++ assets/redirect.05fa133f.js
@@ -0,0 +1,1 @@
+import{h as t,b as a,M as u}from"./vendor.f0a45468.js";const l={setup(){let r=t(),e=a();const{params:o,query:s}=e;return console.log(e),r.replace({name:o.__name,params:o,query:s}),()=>[u("div")]}};export{l as default};
