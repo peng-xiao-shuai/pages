bf6ea8414c816cc432d908d4ac60ff7961641137
Peng_Xiao_Shuai_0902 <1612565136@qq.com>
2021-11-07 19:16:50 +0800

    目的地完成

---
+++ assets/redirect.384077a8.js
@@ -0,0 +1,1 @@
+import{h as t,b as a,P as u}from"./vendor.bb11fbc5.js";const c={setup(){let r=t(),e=a();const{params:o,query:s}=e;return console.log(e),r.replace({name:o.__name,params:o,query:s}),()=>[u("div")]}};export{c as default};
